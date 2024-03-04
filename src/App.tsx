4ccc14e880767789a36884fdc574f1ab763beef7
Marcos Leite <marcosveniciusdjmj2@gmail.com>
2024-03-04 02:50:51 -0300

    feat: added routing and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 export function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 
-import '../styles/auth.scss'; export function NewRoom() {
+import '../styles/auth.scss';
+
+export function NewRoom() {
     return (
         <div id='page-auth'>
             <aside>
@@ -25,7 +29,7 @@ import '../styles/auth.scss'; export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+                        Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
                     </p>
                 </div>
             </main >

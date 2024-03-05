41526ae66b368fcad736166fbb66948d05a252c0
Marcos Leite <marcosveniciusdjmj2@gmail.com>
2024-03-05 00:28:07 -0300

    feat: create room function added

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -7,9 +8,28 @@ import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+import { push, ref } from '@firebase/database';
 
 export function NewRoom() {
     const { user } = useAuth()
+
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        const roomRef = ref(database, 'rooms');
+
+        const firebaseRoom = await push(roomRef, {
+            title: newRoom,
+            authorId: user?.id,
+        })
+    }
 
     return (
         <div id='page-auth'>
@@ -22,10 +42,12 @@ export function NewRoom() {
                 <div className='main-content'>
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input
                             type="text"
                             placeholder='Nome da sala'
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -20,4 +20,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
-export { app as firebase, auth, database}
+export { app as firebase, auth, database }
+
